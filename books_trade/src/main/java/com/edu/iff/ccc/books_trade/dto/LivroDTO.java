457cd3f4768d8341775ab8423f3415a8d0fde485
Gabrielxcb <gabriel.campos@gsuite.iff.edu.br>
2025-08-31 14:50:56 -0300

    Versão atualizada com entidade e service

---
+++ books_trade/src/main/java/com/edu/iff/ccc/books_trade/dto/LivroDTO.java
@@ -0,0 +1,79 @@
+package com.edu.iff.ccc.books_trade.dto;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public class LivroDTO {
+
+    private Long id;
+
+    @NotBlank(message = "O título do livro não pode ser vazio.")
+    @Size(min = 1, max = 100, message = "O título deve ter entre 1 e 100 caracteres.")
+    private String titulo;
+
+    @NotBlank(message = "O autor do livro não pode ser vazio.")
+    @Size(min = 1, max = 100, message = "O nome do autor deve ter entre 1 e 100 caracteres.")
+    private String autor;
+
+    @Size(max = 50, message = "O gênero deve ter no máximo 50 caracteres.")
+    private String genero;
+
+    @Size(max = 255, message = "A descrição deve ter no máximo 255 caracteres.")
+    private String descricao;
+
+    @Min(value = 1, message = "O ano de publicação deve ser maior que 0.")
+    private int anoPublicacao;
+
+    // Getters e Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public int getAnoPublicacao() {
+        return anoPublicacao;
+    }
+
+    public void setAnoPublicacao(int anoPublicacao) {
+        this.anoPublicacao = anoPublicacao;
+    }
+
+
+}
+
