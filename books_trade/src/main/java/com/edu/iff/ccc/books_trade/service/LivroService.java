e7d43d3231a910c522126d4d17b37dac4bb22d72
mariamdsouza <111377072+mariamdsouza@users.noreply.github.com>
2025-09-08 21:18:41 -0300

    Criação do Repository

--- books_trade/src/main/java/com/edu/iff/ccc/books_trade/service/LivroService.java
+++ books_trade/src/main/java/com/edu/iff/ccc/books_trade/service/LivroService.java
@@ -1,47 +1,30 @@
 package com.edu.iff.ccc.books_trade.service;
 
 import com.edu.iff.ccc.books_trade.entities.Livro;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import br.edu.iff.ccc.webdev.entities.Livro;
+import br.edu.iff.ccc.webdev.repository.LivroRepository;
 
 @Service
 public class LivroService {
 
+    @Autowired
+    LivroRepository livroRepository;
     private final List<Livro> livros = new ArrayList<>();
     private long idCounter = 1;
 
     public void saveLivro(Livro livro) {
-        if (livro.getId() == null) {
-            livro.setId(idCounter++);
-        }
-        livros.add(livro);
-        System.out.println("Livro salvo: " + livro.getTitulo() + " - Autor: " + livro.getAutor());
+       livroRepository.saveLivro(livro);
     }
 
     public Livro findLivroById(Long id) {
-        return livros.stream()
-                .filter(l -> l.getId().equals(id))
-                .findFirst()
-                .orElse(null);
+        return livroRepository.findLivroById(id).isPresent() ? livroRepository.findLivroById(id).get() : null;
     }
 
     public List<Livro> findAllLivros() {
-        if (livros.isEmpty()) {
-            Livro livro1 = new Livro("Rainha Vermelha", "Victoria Aveyard", "Fantasia", "Usado", null, "Descrição de Rainha Vermelha", 2015);
-            livro1.setId(idCounter++);
-            livros.add(livro1);
-
-            Livro livro2 = new Livro("A Cor Púrpura", "Alice Walker", "Ficção", "Novo", null, "Descrição de A Cor Púrpura", 1982);
-            livro2.setId(idCounter++);
-            livros.add(livro2);
-
-            Livro livro3 = new Livro("1984", "George Orwell", "Distopia", "Semi-novo", null, "Descrição de 1984", 1949);
-            livro3.setId(idCounter++);
-            livros.add(livro3);
-        }
-        return new ArrayList<>(livros);
+        return (ArrayList<Livro>) livroRepository.findAllLivros;
     }
 }
