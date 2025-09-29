a5544233f6acef4e7e07b42f84cd5ef422bff1c2
mariamdsouza <111377072+mariamdsouza@users.noreply.github.com>
2025-09-29 18:52:47 -0300

    Modificações nas classes de Exceções

--- books_trade/src/main/java/com/edu/iff/ccc/books_trade/exceptions/ApiExceptionHandler.java
+++ books_trade/src/main/java/com/edu/iff/ccc/books_trade/exceptions/ApiExceptionHandler.java
@@ -3,72 +3,127 @@ package com.edu.iff.ccc.books_trade.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import jakarta.servlet.http.HttpServletRequest;
 
 import java.net.URI;
-import java.time.LocalDateTime;
+import java.time.Instant;
 import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
-@ControllerAdvice
-public class ApiExceptionHandler {
+@RestControllerAdvice
+public class RestExceptionHandler {
 
-    @ExceptionHandler({
-        UsuarioNaoEncontradoException.class,
-        LivroNaoEncontradoException.class,
-        PropostaNaoEncontradaException.class,
-        EmailNaoEncontradoException.class
-    })
-    public ProblemDetail handleNotFoundException(HttpServletRequest req, RuntimeException ex) {
-        ProblemDetail pd = ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, ex.getMessage());
-
-        pd.setTitle("Recurso Não Encontrado");
-        pd.setProperty("status", HttpStatus.NOT_FOUND.value());
-        pd.setProperty("message", ex.getMessage());
-        pd.setProperty("exception", ex.getClass().getName());
-        pd.setProperty("timestamp", LocalDateTime.now(ZoneId.systemDefault()).toString());
-        pd.setProperty("path", req.getRequestURI());
-        pd.setInstance(URI.create(req.getRequestURI()));
-
-        return pd;
+    @ExceptionHandler(UsuarioNaoEncontradoException.class)
+    public ProblemDetail handleUsuarioNaoEncontrado(HttpServletRequest req, UsuarioNaoEncontradoException ex) {
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, ex.getMessage());
+        problemDetail.setTitle("Usuário Não Encontrado");
+        
+        problemDetail.setProperty("status", HttpStatus.NOT_FOUND.value());
+        problemDetail.setProperty("message", ex.getMessage());
+        problemDetail.setProperty("exception", ex.getClass().getName());
+        problemDetail.setProperty("timestamp", ZonedDateTime.ofInstant(Instant.now(), ZoneId.systemDefault()).toString());
+        problemDetail.setProperty("path", req.getRequestURI());
+        problemDetail.setInstance(URI.create(req.getRequestURI()));
+        
+        return problemDetail;
     }
 
+    @ExceptionHandler(LivroNaoEncontradoException.class)
+    public ProblemDetail handleLivroNaoEncontrado(HttpServletRequest req, LivroNaoEncontradoException ex) {
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, ex.getMessage());
+        problemDetail.setTitle("Livro Não Encontrado");
+        
+        problemDetail.setProperty("status", HttpStatus.NOT_FOUND.value());
+        problemDetail.setProperty("message", ex.getMessage());
+        problemDetail.setProperty("exception", ex.getClass().getName());
+        problemDetail.setProperty("timestamp", ZonedDateTime.ofInstant(Instant.now(), ZoneId.systemDefault()).toString());
+        problemDetail.setProperty("path", req.getRequestURI());
+        problemDetail.setInstance(URI.create(req.getRequestURI()));
+        
+        return problemDetail;
+    }
+    
+    @ExceptionHandler(PropostaNaoEncontradaException.class)
+    public ProblemDetail handlePropostaNaoEncontrada(HttpServletRequest req, PropostaNaoEncontradaException ex) {
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, ex.getMessage());
+        problemDetail.setTitle("Proposta Não Encontrada");
+        
+        problemDetail.setProperty("status", HttpStatus.NOT_FOUND.value());
+        problemDetail.setProperty("message", ex.getMessage());
+        problemDetail.setProperty("exception", ex.getClass().getName());
+        problemDetail.setProperty("timestamp", ZonedDateTime.ofInstant(Instant.now(), ZoneId.systemDefault()).toString());
+        problemDetail.setProperty("path", req.getRequestURI());
+        problemDetail.setInstance(URI.create(req.getRequestURI()));
+        
+        return problemDetail;
+    }
 
     @ExceptionHandler(EmailJaCadastradoException.class)
-    public ProblemDetail handleEmailJaCadastradoException(HttpServletRequest req, EmailJaCadastradoException ex) {
-        ProblemDetail pd = ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, ex.getMessage());
-
-        pd.setTitle("E-mail Já Cadastrado");
-        pd.setProperty("status", HttpStatus.BAD_REQUEST.value());
-        pd.setProperty("message", ex.getMessage());
-        pd.setProperty("exception", ex.getClass().getName());
-        pd.setProperty("timestamp", LocalDateTime.now(ZoneId.systemDefault()).toString());
-        pd.setProperty("path", req.getRequestURI());
-        pd.setInstance(URI.create(req.getRequestURI()));
-
-        return pd;
+    public ProblemDetail handleEmailJaCadastrado(HttpServletRequest req, EmailJaCadastradoException ex) {
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, ex.getMessage());
+        problemDetail.setTitle("E-mail Já Cadastrado");
+        
+        problemDetail.setProperty("status", HttpStatus.BAD_REQUEST.value());
+        problemDetail.setProperty("message", ex.getMessage());
+        problemDetail.setProperty("exception", ex.getClass().getName());
+        problemDetail.setProperty("timestamp", ZonedDateTime.ofInstant(Instant.now(), ZoneId.systemDefault()).toString());
+        problemDetail.setProperty("path", req.getRequestURI());
+        problemDetail.setInstance(URI.create(req.getRequestURI()));
+        
+        return problemDetail;
     }
 
+    @ExceptionHandler(RegraDeNegocioException.class)
+    public ProblemDetail handleRegraDeNegocio(HttpServletRequest req, RegraDeNegocioException ex) {
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, ex.getMessage());
+        problemDetail.setTitle("Violação de Regra de Negócio");
+        
+        problemDetail.setProperty("status", HttpStatus.BAD_REQUEST.value());
+        problemDetail.setProperty("message", ex.getMessage());
+        problemDetail.setProperty("exception", ex.getClass().getName());
+        problemDetail.setProperty("timestamp", ZonedDateTime.ofInstant(Instant.now(), ZoneId.systemDefault()).toString());
+        problemDetail.setProperty("path", req.getRequestURI());
+        problemDetail.setInstance(URI.create(req.getRequestURI()));
+        
+        return problemDetail;
+    }
+    
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ProblemDetail handleValidationException(HttpServletRequest req, MethodArgumentNotValidException ex) {
+    public ProblemDetail handleValidation(HttpServletRequest req, MethodArgumentNotValidException ex) {
+        
         List<String> errors = ex.getBindingResult().getFieldErrors().stream()
                 .map(error -> error.getField() + ": " + error.getDefaultMessage())
                 .collect(Collectors.toList());
 
-        ProblemDetail pd = ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, "Um ou mais campos estão inválidos.");
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, "Um ou mais campos estão inválidos.");
+        problemDetail.setTitle("Erro de Validação de Dados");
+        
+        problemDetail.setProperty("status", HttpStatus.BAD_REQUEST.value());
+        problemDetail.setProperty("erros_de_campo", errors);
+        problemDetail.setProperty("exception", ex.getClass().getName());
+        problemDetail.setProperty("timestamp", ZonedDateTime.ofInstant(Instant.now(), ZoneId.systemDefault()).toString());
+        problemDetail.setProperty("path", req.getRequestURI());
+        problemDetail.setInstance(URI.create(req.getRequestURI()));
+        
+        return problemDetail;
+    }
 
-        pd.setTitle("Erro de Validação de Dados");
-        pd.setProperty("status", HttpStatus.BAD_REQUEST.value());
-        pd.setProperty("erros_de_campo", errors);
-        pd.setProperty("timestamp", LocalDateTime.now(ZoneId.systemDefault()).toString());
-        pd.setProperty("path", req.getRequestURI());
-        pd.setInstance(URI.create(req.getRequestURI()));
-
-        return pd;
+    @ExceptionHandler(Exception.class)
+    public ProblemDetail handleGeneralError(HttpServletRequest req, Exception ex) {
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.INTERNAL_SERVER_ERROR, "Ocorreu um erro interno no servidor.");
+        problemDetail.setTitle("Erro Interno do Servidor");
+        
+        problemDetail.setProperty("status", HttpStatus.INTERNAL_SERVER_ERROR.value());
+        problemDetail.setProperty("message", ex.getMessage());
+        problemDetail.setProperty("exception", ex.getClass().getName());
+        problemDetaild.setProperty("timestamp", ZonedDateTime.ofInstant(Instant.now(), ZoneId.systemDefault()).toString());
+        problemDetail.setProperty("path", req.getRequestURI());
+        problemDetail.setInstance(URI.create(req.getRequestURI()));
+        
+        return problemDetail;
     }
 }
-
-
--- books_trade/src/main/java/com/edu/iff/ccc/books_trade/exceptions/PropostaNaoEncontradaException.java
+++ books_trade/src/main/java/com/edu/iff/ccc/books_trade/exceptions/PropostaNaoEncontradaException.java
@@ -1,8 +1,5 @@
 package com.edu.iff.ccc.books_trade.exceptions;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.NOT_FOUND)
 public class PropostaNaoEncontradaException extends RuntimeException {
     public PropostaNaoEncontradaException(String message) {
         super(message);
